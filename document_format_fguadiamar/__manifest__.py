facd41ad49cc96f094dfa98e290f7fbdd398dd6c
dani <dani.xtendoo@gmail.com>
2022-11-15 09:34:34 +0100

    añadido document_format

---
+++ document_format_fguadiamar/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Document Format Fguadiamar",
+    "summary": """Formatos de documentos de Ferreteria Guadiamar""",
+    "version": "13.0.1.0.0",
+    "description": """Formatos de documentos de Ferreteria Guadiamar""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "http://xtendoo.es",
+    "category": "Extra Tools",
+    "depends": ["base",],
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/layout/external_layout_clean.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
