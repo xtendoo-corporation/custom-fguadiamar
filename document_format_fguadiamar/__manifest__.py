06dac3b0702977fce7f307d23c20555c9e35bfc0
dani <dani.xtendoo@gmail.com>
2022-11-15 10:00:30 +0100

    sync

--- document_format_fguadiamar/__manifest__.py
+++ document_format_fguadiamar/__manifest__.py
@@ -1,16 +1,16 @@
 {
     "name": "Document Format Fguadiamar",
     "summary": """Formatos de documentos de Ferreteria Guadiamar""",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "description": """Formatos de documentos de Ferreteria Guadiamar""",
     "author": "Daniel Dominguez",
     "company": "Xtendoo",
     "website": "http://xtendoo.es",
     "category": "Extra Tools",
-    "depends": ["base",],
     "license": "AGPL-3",
     "depends": [
         "base",
+        "web"
     ],
     "data": [
         "views/layout/external_layout_clean.xml",
